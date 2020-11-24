adfa15b019be7347dd57b6af0cddf2470bfc8f46
pedrognrd <pedro.gnrd@gmail.com>
2020-11-24 14:16:08 +0100

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Range(1, 10)]
+    public float forceJump = 1;
+    //private bool running = false;
+    [Range(1, 1000)]
+    public float speed = 10;
+    private float x;
+    private float y;
+    Rigidbody2D rigidBody;
+    Animator animator;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Displace();
+
+        if (Mathf.Abs(rigidBody.velocity.y) == 0f)
+        {
+            animator.SetBool("Jumping", false);
+        }
+    }
+
+    void Displace()
+    {
+        rigidBody.velocity = new Vector2(x * Time.deltaTime * speed, rigidBody.velocity.y);
+
+        if (Mathf.Abs(rigidBody.velocity.x) > 0)
+        {
+            animator.SetBool("Running", true);
+            //running = true;
+            
+        }
+        else
+        {
+            animator.SetBool("Running", false);
+            //running = false;
+        }
+    }
+
+    public void Jump()
+    {
+        if (Mathf.Abs(rigidBody.velocity.y) < 0.01f)
+        {
+            rigidBody.AddForce(Vector2.up * forceJump, ForceMode2D.Impulse);
+            animator.SetBool("Jumping", true);
+        }
+    }
+}
---
+++ Assets/_GameAssets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    Animator animator;
+    
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            animator.SetBool("Shooting", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightControl))
+        {
+            animator.SetBool("Shooting", false);
+        }
+    }
+}
