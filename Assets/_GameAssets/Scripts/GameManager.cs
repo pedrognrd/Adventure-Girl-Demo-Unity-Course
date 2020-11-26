c6c74ba0eb235571b4b07fa843f934a61f98d562
pedrognrd <pedro.gnrd@gmail.com>
2020-11-26 21:25:54 +0100

    2020.11.26
    TODO
    - Mobile controls
    	- Scripts for buttons and joystick
    - Enemy attack affects player
    	- Decrease lifes
    
    - GameManager
    
    - Sonidos al disparar tanto de Player como de Ninja Boy
    
    DONE
    - Construir HUD del player
    - Controlar que al disparar a los enemigos para que se active la animación Dying
    - Orientar a Ninja Boy hacia el player

--- Assets/_GameAssets/Scripts/GameManager.cs
+++ Assets/_GameAssets/Scripts/GameManager.cs
@@ -1,18 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // States of the game
+    public enum State { GameOver, Menu, Pause, Playing }
+    // Info Continue Game
+    public static bool continueGame = false;
+    // Capture player object reference
+    private GameObject player;
+    
+
+    [Header("GAME STATE")]
+    // Current State of the game
+    public State state = State.Menu;
+
+    [Header("INVENTORY")]
+    //Inventario
+    public bool hasDiamondBlue = false;
+    public bool hasDiamondGreen = false;
+    public bool hasDiamondRed = false;
+    public bool hasDiamondYellow = false;
+    public bool hasKey = false;
+
+    [Header("POWERS UP")]
+    //--> God Mode, immunity for a while
+    public float godModeTime;
+    public bool godMode = false;
+
+    [Header("HUD CONFIG")]
+    //UI
+    public GameObject prefabLifeImage;
+    private GameObject panelLifes;
+    private Text textScore;
+    private GameObject textGameOver;
+    private GameObject controlsMobile;
+
+    [Header("HUD STATES")]
+    // Current general states of the game
+    public int score;
+    public int lifesNumber;
+    public int maxLifesNumber;
+
+    private void Awake()
     {
-        
+        // Capture object references
+        panelLifes = GameObject.Find("PanelLIfes");
+        textScore = GameObject.Find("TextScore").GetComponent<Text>();
+        textGameOver = GameObject.Find("TextGameOver");
+        textGameOver.SetActive(false);
+        controlsMobile = GameObject.Find("ControlsMobile");
+
+        player = GameObject.Find("Player");
+        score = 0;
+        if (continueGame) RecoverState();
+        textScore.text = score.ToString();
+        lifesNumber = maxLifesNumber;
+
+        //GetComponent<UIManager>().CrearVidasUI(numeroVidas, prefabImagenVida, panelVidas);
+        /*if (UseVJoystick())
+        {
+            mobileControls.SetActive(true);
+        }
+        else
+        {
+            mobileControls.SetActive(false);
+        }*/
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RecoverState()
     {
-        
+        continueGame = false;
+        if (PlayerPrefs.HasKey("Score"))
+        {
+            score = PlayerPrefs.GetInt("Score");
+            if (PlayerPrefs.GetInt("HasKey") == 1)
+            {
+               // RecogerLlave();
+            }
+            player.transform.position = new Vector2(PlayerPrefs.GetFloat("x"), PlayerPrefs.GetFloat("y"));
+        }
     }
+
+    public void KeyTaken()
+    {
+        hasKey = true;
+        //GetComponent<UIManager>().ActivarLlaveUI();
+    }
+
 }
